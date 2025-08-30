419b0e36a364f179bebf3155b21c97a302a52df0
Tom Almeida <tom@tommoa.me>
2025-08-30 23:15:42 +1000

    chore(nixpkgs): Bump flake.lock

---
+++ .config/nvim/lua/tom/plugin/grapple.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "cbochs/grapple.nvim",
+    opts = {
+      icons = false,
+    },
+    keys = {
+      { "<leader>t", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
+      { "<leader>s", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
+
+      { "<c-n>", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
+      { "<c-e>", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
+      { "<c-i>", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
+      { "<c-o>", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+    },
+  }
+}
