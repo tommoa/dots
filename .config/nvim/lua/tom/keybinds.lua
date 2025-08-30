5981abcfc18e8a61e7dd980575a0ec48d476579f
Tom Hill Almeida <tom@tommoa.me>
2025-08-30 11:59:32 +1000

    chore(nvim/lsp): Move LSP keybinds to main keybind directory

--- .config/nvim/lua/tom/keybinds.lua
+++ .config/nvim/lua/tom/keybinds.lua
@@ -48,3 +48,32 @@ mapper("n", "zb", ":Telescope buffers<CR>")
 -- Clear Avante chat history
 mapper("n", "<leader>aC", ":AvanteClear<CR>")
 mapper("n", "<leader>an", ":AvanteChatNew<CR>")
+
+-- LSP keybinds
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(e)
+    local opts = { buffer = e.buf, remap = false }
+
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    vim.keymap.set('n', '<leader>d', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ga', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+    vim.keymap.set("n", "<leader>o", vim.lsp.buf.format, opts)
+    -- Run formatting synchronously before writing a file
+    vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = e.buf,
+        callback = vim.lsp.buf.format,
+    })
+  end
+})
--- .config/nvim/lua/tom/lsp.lua
+++ .config/nvim/lua/tom/lsp.lua
@@ -9,31 +9,7 @@ local capabilities = vim.tbl_deep_extend(
 )
 M.capabilities = capabilities
 
-local on_attach = function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    vim.keymap.set('n', '<leader>d', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
-    vim.keymap.set("n", "<leader>o", vim.lsp.buf.format, opts)
-    -- Run formatting synchronously before writing a file
-    vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = bufnr,
-        callback = vim.lsp.buf.format,
-    })
-end
+local on_attach = function(client, bufnr) end
 M.on_attach = on_attach
 
 M.configs = {}
