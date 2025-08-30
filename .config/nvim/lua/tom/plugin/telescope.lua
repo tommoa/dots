c5ecf0e2a89417468fae567db240f9323d9e542a
Tom Almeida <tom@tommoa.me>
2025-08-30 23:01:39 +1000

    feat(nvim/telescope): Add FZF-native and configure 'ivy' theme for `find_files`

--- .config/nvim/lua/tom/plugin/telescope.lua
+++ .config/nvim/lua/tom/plugin/telescope.lua
@@ -4,7 +4,13 @@ return {
     'nvim-telescope/telescope.nvim',
     version = '0.1.8',
     event = 'VeryLazy',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make'
+      },
+    },
     opts = {
       defaults = {
         mappings = {
@@ -13,8 +19,17 @@ return {
           },
         },
       },
+      pickers = {
+        find_files = {
+          theme = "ivy",
+        },
+      },
+      extensions = {
+        fzf = {},
+      },
     },
-    config = function ()
+    config = function (_, opts)
+      require('telescope').setup(opts)
       local telescope = require('telescope.builtin')
       vim.keymap.set('n', 'zf', telescope.find_files)
       vim.keymap.set('n', 'zb', telescope.buffers)
