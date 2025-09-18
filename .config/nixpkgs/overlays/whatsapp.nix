6e6f8360323e40a4c5fc9607bd82c5b521672e08
Tom Almeida <tom@tommoa.me>
2025-09-19 07:16:56 +1000

    chore(nix): Update WhatsApp to 2.25.24.76

--- .config/nixpkgs/overlays/whatsapp.nix
+++ .config/nixpkgs/overlays/whatsapp.nix
@@ -1,11 +1,11 @@
 self: super: {
   whatsapp-for-mac = super.whatsapp-for-mac.overrideAttrs (o: rec {
-    version = "2.25.23.83";
+    version = "2.25.24.76";
     src = super.fetchzip {
       extension = "zip";
       name = "WhatsApp.app";
       url = "https://web.whatsapp.com/desktop/mac_native/release/?version=${version}&extension=zip&configuration=Release&branch=relbranch";
-      hash = "sha256-mkJFfPhfDFJBFL6qcMMlGC/u1+OT+dcIiKadkEfRn4Q=";
+      hash = "sha256-fzLV/u7TDVp/LWRTaztsX4YxyCZ++mXOKI2nyK7J8F8=";
     };
   });
 }
