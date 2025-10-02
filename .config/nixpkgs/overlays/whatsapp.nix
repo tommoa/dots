f737571fdd10cb944a17a45ec5079f9165cd4210
Tom Almeida <tom@tommoa.me>
2025-10-02 18:36:32 +0800

    chore(nixpkgs): Bump WhatsApp for mac to 2.25.28.17

--- .config/nixpkgs/overlays/whatsapp.nix
+++ .config/nixpkgs/overlays/whatsapp.nix
@@ -1,11 +1,11 @@
 self: super: {
   whatsapp-for-mac = super.whatsapp-for-mac.overrideAttrs (o: rec {
-    version = "2.25.24.76";
+    version = "2.25.28.17";
     src = super.fetchzip {
       extension = "zip";
       name = "WhatsApp.app";
-      url = "https://web.whatsapp.com/desktop/mac_native/release/?version=${version}&extension=zip&configuration=Release&branch=relbranch";
-      hash = "sha256-fzLV/u7TDVp/LWRTaztsX4YxyCZ++mXOKI2nyK7J8F8=";
+      url = "https://web.whatsapp.com/desktop/mac_native/release/?version=${version}&extension=zip&configuration=Release";
+      hash = "sha256-Mc8+d8Kb58kfJ61Qy38mgItwtNkz1bdKhf6J09bawQ0=";
     };
   });
 }
